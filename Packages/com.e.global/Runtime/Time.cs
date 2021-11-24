0e3a7a2affe1c5fb69a7ecd2e81b66822705908a
ZhanQI <chinese778@foxmail.com>
2021-11-25 00:39:12 +0800

    add Time

---
+++ Packages/com.e.global/Runtime/Time.cs
@@ -0,0 +1,13 @@
+namespace E
+{
+    /// <summary>
+    /// Time information.
+    /// </summary>
+    public static class Time
+    {
+        public static double time;
+        public static double deltaTime;
+        public static double realTime;
+
+    }
+}
